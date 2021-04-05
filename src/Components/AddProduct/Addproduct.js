7d9b19b8c7e8b9abdd9acea6a94cb2a87075416a
min23a <minhazuloabedin0611@gmail.com>
2021-04-05 23:22:03 +0600

    Updated admin

--- src/Components/AddProduct/Addproduct.js
+++ src/Components/AddProduct/Addproduct.js
@@ -2,22 +2,23 @@ import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import "./Addproduct.css"
 
 const Addproduct = () => {
-    const [product,setProduct] = useState({
-        productName : '',
-        productPhoto : '',
-        price : ''
+    const [product, setProduct] = useState({
+        productName: '',
+        productPhoto: '',
+        price: ''
     })
     const handleChange = (e) => {
-        const newProduct = {...product};
+        const newProduct = { ...product };
         newProduct[e.target.name] = e.target.value;
         setProduct(newProduct);
     }
-    const handleChangeImg = (e) =>{
+    const handleChangeImg = (e) => {
         console.log(e.target.files[0])
         const imgData = new FormData();
-        imgData.set('key','0fb03a882241f5c04128b1b8370fe608')
+        imgData.set('key', '0fb03a882241f5c04128b1b8370fe608')
         imgData.append('image', e.target.files[0])
         axios.post('https://api.imgbb.com/1/upload', imgData)
             .then(function (response) {
@@ -37,22 +38,22 @@ const Addproduct = () => {
                 console.log(error);
             });
     }
-    
+
     return (
-            <div className="w-100">
-                <div className="row">
-                    <div className="col-md-3 bg-dark">
-                        <li><Link to='/addProduct'>Add Product</Link></li>
-                        <li><Link to='/manageProduct'>Manage Product</Link></li>
-                    </div>
-                    <div className="col-md-9">
-                        <input onBlur={handleChange} type="text" name="productName" id="productName" />
-                        <input onChange={handleChangeImg} type="file" name="productPhoto" id="productPhoto" />
-                        <input onBlur={handleChange} type="number" name="price" id="price" />
-                        <input onClick={handleSubmit} type="submit" />
-                    </div>
+        <div className="w-100">
+            <div className="row">
+                <div className="col-md-3 bg-dark adminNav">
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/addProduct'>Add Product</Link></li>
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/manageProduct'>Manage Product</Link></li>
+                </div>
+                <div className="col-md-9">
+                    <input onBlur={handleChange} type="text" name="productName" id="productName" placeholder="Product Name..." /><br/><br/>
+                    <input onChange={handleChangeImg} type="file" name="productPhoto" id="productPhoto" /><br/><br/>
+                    <input onBlur={handleChange} type="number" name="price" id="price" placeholder="Price..." /><br/><br/>
+                    <input className="btn-success" onClick={handleSubmit} type="submit" />
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/Components/ManageProduct/ManageProduct.js
+++ src/Components/ManageProduct/ManageProduct.js
@@ -2,35 +2,41 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './ManageProduct.css';
 
 const ManageProduct = () => {
-    const [products,setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('https://lychee-custard-72086.herokuapp.com/products')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     const handleClick = (product_id) => {
         const url = `https://lychee-custard-72086.herokuapp.com/delete/${product_id}`
         console.log(url);
-        // fetch(url, { method: 'DELETE' })
-        //     .then(res => console.log(res));
+        fetch(url, { method: 'DELETE' })
+            .then(res => console.log(res));
+        const productLi = document.getElementById(product_id);
+        productLi.classList.add('d-none')
     }
     return (
         <div className="w-100">
             <div className="row">
-                <div className="col-md-3 bg-dark">
-                    <li><Link to='/addProduct'>Add Product</Link></li>
-                    <li><Link to='/manageProduct'>Manage Product</Link></li>
+                <div className="col-md-3 bg-dark adminNav">
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/addProduct'>Add Product</Link></li>
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/manageProduct'>Manage Product</Link></li>
                 </div>
                 <div className="col-md-9">
-                        {
-                            products.map(product =>
-                                <li key={product._id} style={{fontSize : '24px',listStyle : 'none'}}>
-                                    Name : {product.productName}<br />Price : $ {product.price}
-                                    <button onClick={ () =>handleClick(product._id)}  className="btn btn-danger ml-3 text-white"><FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon></button>
-                                </li>)
-                        }
+                    {
+                        products.map(product =>
+                            <li key={product._id} id={product._id} className="d-flex justify-content-between mt-2" style={{ fontSize: '24px', listStyle: 'none' }}>
+                                <span className="text-success">Name : {product.productName}   </span>
+                                <span className="text-primary" > Price : $ {product.price}</span>
+                                <button onClick={() => handleClick(product._id)} className="btn btn-danger text-white">
+                                    <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+                                </button>
+                            </li>)
+                    }
                 </div>
             </div>
         </div>
