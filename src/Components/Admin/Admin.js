1b84bb8b0ba0bf780a068d5e1a63398e54cb4288
min23a <minhazuloabedin0611@gmail.com>
2021-04-05 23:35:19 +0600

    updated admin component and title

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Addproduct from '../AddProduct/Addproduct';
 
 const Admin = () => {
     return (
         <div className="w-100">
             <div className="row">
-                <div className="col-md-3 bg-dark">
-                    <li><Link to='/addProduct'>Add Product</Link></li>
-                    <li><Link to='/manageProduct'>Manage Product</Link></li>
+                <div className="col-md-3 bg-dark adminNav">
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/addProduct'>Add Product</Link></li>
+                    <li className="m-3" style={{ fontSize: '24px', listStyle: 'none' }}><Link to='/manageProduct'>Manage Product</Link></li>
                 </div>
                 <div className="col-md-9">
-
+                    <Addproduct/>
                 </div>
             </div>
         </div>
